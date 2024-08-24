3fd451902713a9316ee56bc4724376cf4a4cdabc
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-08-24 12:59:24 +0900

    run express serving with pug template engine

--- src/server.js
+++ src/server.js
@@ -2,6 +2,22 @@ import express from "express"
 
 const app = express()
 
-console.log("helloworld")
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+app.use("/public", express.static(__dirname + "/public"))
 
-app.listen(3000);
+app.get("/", (
+  _,
+  res) => {
+  res.render("home");
+});
+
+// fallback
+app.get("/*", (
+  _,
+  res) => {
+  res.redirect("/");
+})
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
