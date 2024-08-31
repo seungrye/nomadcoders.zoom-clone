4cb1a39bd1e5ddc279796a33d91828084c7a7e63
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-09-01 00:51:17 +0900

    방에 입장전에 닉네임 설정 가능하도록 처리

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,6 @@
 const socket = io();
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const enterForm = welcome.querySelector("#enter");
 const room = document.getElementById("room");
 
 room.hidden = true;
@@ -13,9 +13,16 @@ const addMessage = (message) => {
   ul.appendChild(li)
 };
 
-const handleRoomSubmit = (event) => {
+const nameForm = welcome.querySelector("#name");
+nameForm.onsubmit = (event) => {
   event.preventDefault();
-  const input = form.querySelector("input");
+  const input = nameForm.querySelector("input");
+  socket.emit("nickname", { payload: input.value });
+};
+
+enterForm.onsubmit = (event) => {
+  event.preventDefault();
+  const input = enterForm.querySelector("input");
 
   socket.emit("enter-room", { payload: input.value }, () => {
     welcome.hidden = true;
@@ -23,13 +30,6 @@ const handleRoomSubmit = (event) => {
 
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
-
-    const nameForm = room.querySelector("#name");
-    nameForm.onsubmit = (event) => {
-      event.preventDefault();
-      const input = nameForm.querySelector("input");
-      socket.emit("nickname", { payload: input.value });
-    };
 
     const msgForm = room.querySelector("#msg");
     msgForm.onsubmit = (event) => {
@@ -44,9 +44,7 @@ const handleRoomSubmit = (event) => {
 
   roomName = input.value;
   input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit)
+};
 
 socket.on("hello", (nickname) => {
   addMessage(`${nickname} joined`);
